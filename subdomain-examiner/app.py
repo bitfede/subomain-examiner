2c4685ee4012158e9184224cb757bc05ea928795
Fede Defaveri <f.g.defaveri@gmail.com>
2019-07-16 17:01:24 +0200

    Simplifies the args inputs and lays out a basic structure

--- subdomain-examiner/app.py
+++ subdomain-examiner/app.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 ###
 # AUTHOR: Federico De Faveri
 # DATE: July 2019
@@ -5,27 +7,37 @@
 ###
 
 import argparse
-
+import os
 
 parser = argparse.ArgumentParser()
 
 # get arguments from command line
-parser.add_argument("-i", help="pathname of the INPUT file")
-parser.add_argument("-o", help="pathname of the OUTPUT file")
+parser.add_argument("-i", help="pathname of the INPUT file, a list of subdomains")
 
 args = parser.parse_args()
 
 inputfile = None
-outputfile = None
 
 inputfile = args.i
-outputfile = args.o
 
-if inputfile is None or outputfile is None:
+if inputfile is None:
     print("[!] No inputfile or outputfile flag specified")
-    print("[i] Usage: subdomain-examiner -i path/to/inputfile -o path/to/outputfile")
+    print("[i] Usage: subdomain-examiner -i path/to/inputfile")
     print("[!] Exiting")
     exit(1)
+else:
+    print(f"[*] Reading subdomains from: {inputfile}")
 
+# check if they are valid files
+is_inputfile_valid = os.path.isfile(inputfile)
 
-print(f"input: {inputfile} | output: {outputfile}")
+print(f"1 {is_inputfile_valid}")
+
+# parse the file and get an array of subdomains
+
+#for each subdomain call the scanner method/s
+
+#get back structured results and call reporting method/s
+
+#exit gracefully
+exit(0)
