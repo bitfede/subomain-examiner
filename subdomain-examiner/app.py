12a21ec8398a7ab389abc9aaac936e4a4e17bd1c
Fede Defaveri <f.g.defaveri@gmail.com>
2019-07-16 05:26:26 +0200

    Lays down basic python module files

--- subdomain-examiner/app.py
+++ subdomain-examiner/app.py
@@ -10,3 +10,22 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # get arguments from command line
+parser.add_argument("-i", help="pathname of the INPUT file")
+parser.add_argument("-o", help="pathname of the OUTPUT file")
+
+args = parser.parse_args()
+
+inputfile = None
+outputfile = None
+
+inputfile = args.i
+outputfile = args.o
+
+if inputfile is None or outputfile is None:
+    print("[!] No inputfile or outputfile flag specified")
+    print("[i] Usage: subdomain-examiner -i path/to/inputfile -o path/to/outputfile")
+    print("[!] Exiting")
+    exit(1)
+
+
+print(f"input: {inputfile} | output: {outputfile}")
