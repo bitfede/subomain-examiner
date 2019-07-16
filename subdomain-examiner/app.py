0b2cebb58fb942b556cceac996afc1a60c51b4d9
Fede Defaveri <f.g.defaveri@gmail.com>
2019-07-16 05:11:08 +0200

    Lays down basic python module files

---
+++ subdomain-examiner/app.py
@@ -0,0 +1,12 @@
+###
+# AUTHOR: Federico De Faveri
+# DATE: July 2019
+# PURPOSE: Python tool to perform recon on a list of subdomains
+###
+
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+# get arguments from command line
